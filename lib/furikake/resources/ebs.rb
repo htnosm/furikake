57bd3d011c3e9b2de62be7e66301c9b9ab29a506
htnosm <htnosm@gmail.com>
2020-08-22 16:38:51 +0900

    Add ebs resource

---
+++ lib/furikake/resources/ebs.rb
@@ -0,0 +1,61 @@
+module Furikake
+  module Resources
+    module Ebs
+      def report(format = nil)
+        volumes = get_resources
+        contents = {
+          title: 'EBS',
+          resources: [
+            {
+               subtitle: '',
+               header: ['Name', 'Volume ID', 'Size(GiB)', 'Volume Type', 'IOPS',
+                        'Availability Zone', 'State',
+                        'Attachment information'],
+               resource: volumes.sort
+            }
+          ]
+        }
+        Furikake::Formatter.shaping(format, contents)
+      end
+
+      def get_resources
+        ec2 = Aws::EC2::Client.new
+        params = {}
+        volumes = []
+        loop do
+          res = ec2.describe_volumes(params)
+          res.volumes.each do |v|
+            volume = []
+            volume << 'N/A' if v.tags.map(&:to_h).all? { |h| h[:key] != 'Name' }
+            v.tags.each do |tag|
+              volume << tag.value if tag.key == 'Name'
+            end
+            volume << v.volume_id
+            volume << v.size
+            volume << v.volume_type
+            volume << v.iops
+            volume << v.availability_zone
+            volume << v.state
+
+            attachments = []
+            v.attachments.each do |a|
+              attachment = []
+              attachment << a.instance_id
+              attachment << a.device
+              attachment << a.state
+              attachments << attachment.join(':')
+            end
+            volume << attachments.join('<br>')
+
+            volumes << volume
+          end
+          break if res.next_token.nil?
+          params[:next_token] = res.next_token
+        end
+
+        volumes
+      end
+      module_function :report, :get_resources
+    end
+  end
+end
---
+++ lib/furikake/resources/ec2_with_vol.rb
@@ -0,0 +1,76 @@
+module Furikake
+  module Resources
+    module Ec2WithVol
+      def report(format = nil)
+        instance = get_resources
+        contents = {
+          title: 'EC2',
+          resources: [
+            {
+               subtitle: '',
+               header: ['Name', 'Instance ID', 'Instance Type',
+                        'Availability Zone', 'Private IP Address',
+                        'Public IP Address', 'State',
+                        'Volume(device:volume_id:volume_size)'],
+               resource: instance.sort
+            }
+          ]
+        }
+        Furikake::Formatter.shaping(format, contents)
+      end
+
+      def get_resources
+        ec2 = Aws::EC2::Client.new
+        params = {}
+        instances = []
+        loop do
+          res = ec2.describe_instances(params)
+          res.reservations.each do |r|
+            r.instances.each do |i|
+              instance = []
+              instance << 'N/A' if i.tags.map(&:to_h).all? { |h| h[:key] != 'Name' }
+              i.tags.each do |tag|
+                instance << tag.value if tag.key == 'Name'
+              end
+              instance << i.instance_id
+              instance << i.instance_type
+              instance << i.placement.availability_zone
+              instance << i.private_ip_address
+              if i.public_ip_address.nil?
+                instance << ' '
+              else
+                instance << i.public_ip_address
+              end
+              instance << i.state.name
+
+              volume_ids = []
+              i.block_device_mappings.each do |b|
+                volume_ids << b.ebs.volume_id
+              end
+              params = {
+                volume_ids: volume_ids,
+              }
+              vol_res = ec2.describe_volumes(params)
+              volumes = []
+              vol_res.volumes.each do |v|
+                volume = []
+                volume << v.attachments[0].device
+                volume << v.volume_id
+                volume << v.size
+                volumes << volume.join(':')
+              end
+              instance << volumes.sort.join('<br>')
+
+              instances << instance
+            end
+          end
+          break if res.next_token.nil?
+          params[:next_token] = res.next_token
+        end
+
+        instances
+      end
+      module_function :report, :get_resources
+    end
+  end
+end
