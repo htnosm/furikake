0568e891331d2164769dbbbe7f530ceccb629778
htnosm <htnosm@gmail.com>
2020-10-19 11:37:14 +0900

    fix redshfit

--- lib/furikake/resources/redshift.rb
+++ lib/furikake/resources/redshift.rb
@@ -29,9 +29,8 @@ module Furikake
           cluster << c[:node_type]
           cluster << "#{c[:cluster_status]}(#{c[:cluster_availability_status]})"
           cluster << c[:number_of_nodes]
-          # Add space to both end for backlog markdown table parse
-          cluster << " jdbc:redshift://#{endpoint} "
-          odbc_url = " Driver={Amazon Redshift (x64)}; Server=#{c[:endpoint][:address]}; Database=#{c[:db_name]};UID=#{c[:master_username]}; PWD=insert_your_master_user_password_here; Port=#{c[:endpoint][:port]} "
+          cluster << "`jdbc:redshift://#{endpoint}`"
+          odbc_url = "`Driver={Amazon Redshift (x64)}; Server=#{c[:endpoint][:address]}; Database=#{c[:db_name]};UID=#{c[:master_username]}; PWD=insert_your_master_user_password_here; Port=#{c[:endpoint][:port]}`"
           cluster << odbc_url
           cluster_infos << cluster
         end
